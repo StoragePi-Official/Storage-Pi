e95646721f349896b72bb884412b3b17dadb7a4b
StoragePi-Official <167295741+StoragePi-Official@users.noreply.github.com>
2024-04-17 11:31:46 +0200

    Update uploadFile.php

--- uploadFile.php
+++ uploadFile.php
@@ -16,10 +16,7 @@ if (isset($_FILES['file']) && !empty($filePath)) {
 
     // Create directory if it doesn't exist
     if (!file_exists($uploadDir)) {
-        if (!mkdir($uploadDir, 0777, true)) {
-            echo "Error: Failed to create directory.";
-            exit;
-        }
+        mkdir($uploadDir, 0777, true);
     }
 
     // Include the file name in the upload directory path
@@ -27,14 +24,15 @@ if (isset($_FILES['file']) && !empty($filePath)) {
 
     // Upload the file
     if (move_uploaded_file($file['tmp_name'], $targetFile)) {
-        // Change ownership to root
-        if (!chown($targetFile, 'root')) {
-            echo "Error: Failed to change ownership to root.";
-        } else {
+        // Check if file uploaded successfully
+        if (file_exists($targetFile)) {
             echo "File uploaded successfully.";
+        } else {
+            echo "Error: File upload failed.";
         }
     } else {
-        echo "Error uploading file: " . $_FILES["file"]["error"];
+        // Display upload error
+        echo "Error uploading file: " . $file['error'];
     }
 } else {
     echo "Invalid file path.";
