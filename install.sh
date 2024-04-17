42752d381e319f2348a5431d7bdd24fe1c4de1c7
StoragePi-Official <167295741+StoragePi-Official@users.noreply.github.com>
2024-04-17 13:09:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,34 @@
 
 # Function to print a green checkmark
 print_green_checkmark() {
-    printf "\e[32m\u2713\e[0m"
+    printf "[\e[32m\u2713\e[0m]"
+}
+
+# Function to print an icon indicating an action
+print_action_icon() {
+    printf "[\e[33m$1\e[0m]"
+}
+
+# Function to print a separator line
+print_separator_line() {
+    cols=$(tput cols)
+    sep=""
+    for ((i=1; i<=cols; i++)); do
+        sep="${sep}-"
+    done
+    printf -- "$sep\n"
 }
 
 # Check if Apache2 is installed
 if ! command -v apache2 &> /dev/null
 then
-    echo "Apache2 is not installed. Installing..."
+    print_separator_line
+    echo -n "$(print_action_icon '+') "
+    echo "Installing Apache2..."
     sudo apt-get update
     sudo apt-get install -y apache2
 else
+    print_separator_line
     echo -n "$(print_green_checkmark) "
     echo "Apache2 is already installed."
 fi
@@ -19,10 +37,13 @@ fi
 # Check if PHP is installed
 if ! command -v php &> /dev/null
 then
-    echo "PHP is not installed. Installing..."
+    print_separator_line
+    echo -n "$(print_action_icon '+') "
+    echo "Installing PHP..."
     sudo apt-get update
     sudo apt-get install -y php
 else
+    print_separator_line
     echo -n "$(print_green_checkmark) "
     echo "PHP is already installed."
 fi
@@ -33,9 +54,13 @@ cd /var/www/html
 # Check if Storage-Pi directory exists, if not create it
 if [ ! -d "Storage-Pi" ]
 then
+    print_separator_line
+    echo -n "$(print_action_icon '+') "
     echo "Creating Storage-Pi directory..."
     mkdir Storage-Pi
 else
+    print_separator_line
+    echo -n "$(print_green_checkmark) "
     echo "Storage-Pi directory already exists."
 fi
 
@@ -45,9 +70,13 @@ cd Storage-Pi
 # Clone the repository if it's not already cloned
 if [ ! -d ".git" ]
 then
+    print_separator_line
+    echo -n "$(print_action_icon '+') "
     echo "Cloning Storage-Pi repository..."
     git clone https://github.com/StoragePi-Official/Storage-Pi.git .
 else
+    print_separator_line
+    echo -n "$(print_green_checkmark) "
     echo "Storage-Pi repository is already cloned."
 fi
 
